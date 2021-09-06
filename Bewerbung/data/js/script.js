c70a2d077abd31f0e44c9a35cd12dcc1121626d7
Janne Sickert <58070646+JanneSickert@users.noreply.github.com>
2021-09-06 14:20:23 +0200

    Ask for cookies

--- Bewerbung/data/js/script.js
+++ Bewerbung/data/js/script.js
@@ -1,3 +1,16 @@
+/*
+    Cookie information
+    ------------------
+    localStorage:
+        ID 66:
+            If a button is pressed on a certificate, 
+            the respective certificate is addressed as a cookie and then loaded in the reload process. 
+            The file name is not saved in the cookie object but in localStorage. 
+            This method also saves data in the browser using an ID.
+        ID 65:
+            Store 1 if cookie was accept null value if not.
+*/
+
 const ROOT = "../../pics/"
 const URLS = {
     abi: ROOT + "AbiZeugnis.jpg",
@@ -9,8 +22,10 @@ const URLS = {
 var aWriteFunction = null
 var setFunction = null
 var getFunction = null
+var deleteFunction = null
 const HTML_ID = "bildZeugnis"
 const STORAGE_ID = "66"
+const ACCEPT_COOKIE = "65"
 
 function makeCommand(url) {
     return ("<img id=\"" + HTML_ID + "\" src=\"" + url + "\" alt=''>")
@@ -56,10 +71,47 @@ function j3() {
 	aWriteFunction(makeCommand(URLS.j3))
 }
 
-function main(writeFunction, sf, gf) {
+var noCookies = function(pDocument, language) {
+    const PIC_PATH = "/Bewerbung/data/pics/"
+    var message = {
+        de: "Wenn sie die Cookies ablehnen können sie die restlichen"
+         + " Zeugnisse im Verzeichniss " + PIC_PATH + " einsehen. "
+         + "Schreibe ja um alle Cookies zu löschen.",
+        en: "If you reject the cookies you can see the remaining "
+         + "certificates in the directory " + PIC_PATH + " Write yes to delete all cookies."
+    }
+    if (language === "de") {
+        var res = prompt(message.de)
+        if (res === "ja") {
+            deleteFunction()
+            alert("Alle Cookies wurden gelöscht.");
+        } else {
+            setFunction(ACCEPT_COOKIE, "1")
+            alert("Alle Cookies wurden beibehalten.")
+        }
+    } else {
+        var res = prompt(message.en)
+        if (res === "yes") {
+            deleteFunction()
+            alert("All cookies have been deleted.");
+        } else {
+            setFunction(ACCEPT_COOKIE, "1")
+            alert("All cookies were retained.")
+        }
+    }
+    pDocument.getElementById('mbmcookie').style.display = 'none';
+}
+
+var yesCookies = function(pDocument) {
+    setFunction(ACCEPT_COOKIE, "1")
+    pDocument.getElementById('mbmcookie').style.display = 'none';
+}
+
+function main(writeFunction, sf, gf, df) {
     aWriteFunction = writeFunction;
     setFunction = sf
     getFunction = gf
+    deleteFunction = df
     var value = getFunction(STORAGE_ID)
     if (value === "j1") {
         j1()
@@ -69,5 +121,11 @@ function main(writeFunction, sf, gf) {
 		j3()
     } else {
         abi();
+    }
+}
+
+function checkSetCookies(pDocument) {
+    if (getFunction(ACCEPT_COOKIE) === "1") {
+        pDocument.getElementById('mbmcookie').style.display = 'none'
     }
 }
